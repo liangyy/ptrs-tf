527b020a580d07cfd5dc5b5f86e700bfb858bbc5
Yanyu Liang <yanyul@uchicago.edu>
2020-01-23 08:22:22 -0600

    added two elastic net lib

---
+++ code/lib_ElasticNet.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+
+class ElasticNet:
+    def __init__(self, num_predictors, alpha, lambda_, A = None, b = None, seed = None):
+        init = tf.random_normal_initializer(seed = seed)
+        if A is None:
+            A = tf.Variable(init(shape = [num_predictors, 1]), name = 'A')
+        if b is None:
+            b = tf.Variable(init(shape = [1, 1]), name = 'b')
+        self.A = A
+        self.b = b
+        self.trainable_variables = [self.A, self.b]
+        self.proximal_variables = [self.A]
+        self.not_prox_variables = [self.b]
+        self.alpha = alpha
+        self.lambda_ = lambda_
+        l1, l2 = self.lambda_alpha_to_l1_l2(alpha, lambda_)
+        update_l1_l2(l1, l2)
+    
+    def update_l1_l2(self, l1, l2):
+        self.l1 = tf.constant(tf.cast(l1, tf.float32))
+        self.l2 = tf.constant(tf.cast(l2, tf.float32))
+    
+    def lambda_alpha_to_l1_l2(self, alpha, lambda_):
+        l1 = alpha * lambda_
+        l2 = (1 - alpha) * lambda_
+        return l1, l2
+    
+    def forward(self, x):
+        return tf.tensordot(x, self.A, axes = [1, 0]) + self.b
+
+    def regu_l1(self):
+        regu_l1 = tf.reduce_sum(tf.abs(self.A))
+        return tf.multiply(self.l1, regu_l1)
+
+    def regu_l2(self):
+        regu_l2 = tf.reduce_sum(tf.square(self.A))
+        return tf.multiply(self.l2, regu_l2)
+
+    def regularization(self):
+        return tf.add(self.regu_l1(), self.regu_l2()) 
+
+    def loss(self, x, y):
+        return tf.reduce_mean(tf.square(y - self.forward(x)))
+
+    def objective(self, x, y):
+        tmp = self.loss(x, y)
+        return tf.add(tmp, self.regularization()), tmp
+
+    def proximal_obj(self, x, y):
+        tmp = self.loss(x, y)
+        return tf.add(tmp, self.regu_l2()), tmp
+
+    def update_lambda(self, lambda_):
+        l1, l2 = self.lambda_alpha_to_l1_l2(alpha, lambda_)
+        self.lambda_ = lambda_
+        update_l1_l2(l1, l2)
+
+class ProximalUpdater:  # it is not called optimizer to distinguish from the Optimizer class in TF2
+    def __init__(self, learning_rate = None):
+        self.learning_rate = learning_rate
+    
+    def prox_l1(self, x, lambda_t):
+        sign = tf.math.sign(x)
+        x_n_lambda_t = tf.add(x, tf.multiply(sign, tf.negative(lambda_t)))
+        # apply soft thresholding
+        sign_new = tf.math.sign(x_n_lambda_t)
+        sign_preserve = tf.math.equal(sign, sign_new) 
+        return tf.add(
+            tf.multiply(tf.cast(sign_preserve, tf.float32), x_n_lambda_t), 
+            tf.multiply(tf.cast(tf.logical_not(sign_preserve), tf.float32), tf.zeros(x.shape))
+        )
+    
+    def proximal_update(self, pairs_prox, pairs_not_prox, l1):              
+        if self.learning_rate is not None:
+            col = []
+            for grad, var in pairs_not_prox:
+                var.assign(var - tf.multiply(self.learning_rate, grad))
+            for grad, var in pairs_prox:
+                tmp_prox = var - tf.multiply(self.learning_rate, grad)
+                var.assign(self.prox_l1(tmp_prox, tf.multiply(l1, learning_rate)))
+            return col
+        else:
+            # TODO: add backtrack line search to determine step size 
+            pass
+            
+    def proximal_train_step(self, model, x, y):
+    #     for x, y in dataset:
+        with tf.GradientTape() as tape:
+            obj, loss = model.proximal_obj(x, y)
+        grad = tape.gradient(obj, [model.proximal_variables, model.not_prox_variables])
+        grad_prox = grad[0]
+        grad_not_prox = grad[1]
+        proximal_update(
+            zip(grad_prox, model.proximal_variables), 
+            zip(grad_not_prox, model.not_prox_variables), 
+            model.l1, 
+            learning_rate = self.learning_rate
+        )
+        return loss, obj
---
+++ code/util_ElasticNet.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+import tensorflow_io as tfio
+import numpy as np
+import h5py
+
+# elastic net math related
+def get_lambda_max(model, x, y):
+    '''
+    `model` is object of ElasticNet class
+    return lambda_max given dataset x, y and alpha of ElasticNet model 
+    '''
+    a = model.A
+    b = model.b
+    model.A.assign(tf.zeros(a.shape))
+    model.b.assign(tf.ones(b.shape), tf.reduce_mean(y))
+    with tf.GradientTape() as tape:
+        obj, loss = model.proximal_obj(x, y)
+    grad = tape.gradient(obj, [model.proximal_variables, model.not_prox_variables])
+    l1_max = tf.reduce_max(tf.abs(grad[0]))
+    lambda_max = l1_max / model.alpha
+    return lambda_max
+    
+def get_lambda_sequence(lambda_max, lambda_min, nlambda):
+    '''
+    return a sequence with equal space in log scale (numpy array) 
+    '''
+    seq = np.linspace(np.log(lambda_max), np.log(lambda_min), num = nlambda)
+    return np.exp(seq)
+# END --
+
+# train_step for built-in optimizer
+def train_step(model, x, y, optimizer):
+    with tf.GradientTape() as tape:
+        obj, loss = model.objective(x, y)
+    grad = tape.gradient(obj, model.trainable_variables)
+    optimizer.apply_gradients(zip(grad, model.trainable_variables))
+    return loss, obj
+# END --
+
+# data I/O
+def load_hdf5_as_dataset(filename_list, dataset_list, batch_size, num_epochs, shuffle = None, take = None):
+    '''
+    `filename_list` and `dataset_list` should be file path and dataset name of x and y
+    It maps all numbers to tf.float32 using tf.cast
+    The value in `shuffle` specifies the `buffer_size` 
+    '''
+    # get to know the size of dataset 
+    h5handle = h5py.File(filename_list[0], 'r')
+    size = h5handle[dataset_list[0]].shape[0]
+    h5handle.close()
+    # load dataset ad TF Dataset
+    X = tfio.IODataset.from_hdf5(filename_list[0], dataset_list[0])
+    y = tfio.IODataset.from_hdf5(filename_list[1], dataset_list[1])
+    if take is not None:
+        X = X.take(take)
+        y = y.take(take)
+    dataset = tf.data.Dataset.zip((X, y))
+    dataset = dataset.map(lambda x , y : (tf.cast(x, tf.float32), tf.cast(y, tf.float32)))
+    if shuffle is not None:
+        dataset = dataset.shuffle(shuffle)
+    dataset = dataset.batch(batch_size).repeat(num_epochs)
+    return dataset, size
+def load_hdf5_as_tensor(filename, dataset_list):
+    test_data = tfio.IOTensor.from_hdf5(filename_list)
+    out_list = []
+    for dataset in dataset_list:
+        out_list.append(tf.cast(test_data(dataset).to_tensor(), tf.float32))
+    return out_list
+def load_predixcan_hdf5(filename, dataset_sample, dataset_gene, dataset_matrix, training_samples, tensor_samples_list):
+    '''
+    training data will be loaded as TF2 Dataset.
+    tensor data will be loaded as TF2 Tensor (in memory). 
+    '''
+    h5handle = h5py.File(filename, 'r')
+    sample_names = h5handle[dataset_sample][:]
+    
+    X = tfio.IODataset.from_hdf5(filename, dataset_list[0])
+    return d
+# END --
+
