18b3be606785e5f558052b893ca9a376ea4a1d0f
Yanyu Liang <yanyul@uchicago.edu>
2020-02-04 11:24:08 -0600

    working on mesa pipe

---
+++ code/util_misc.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def intersect_indice(set1, set2):
+    '''
+    Return the indices of element in intersect of set1 and set2.
+    The ordering of indices result in the same element order.
+    Example:
+        input:
+            set1 = [1,3,4,5]
+            set2 = [5,6,7,1]
+        output:
+            indice1 = [0,3]
+            indice2 = [3,0]
+    '''
+    set_both = np.intersect1d(set1, set2)
+    return _extract_subset_indice_with_sorting(set1, set_both), _extract_subset_indice_with_sorting(set2, set_both)
+def _extract_subset_indice_with_sorting(extract_from, target_set):
+    sorted_idx = np.argsort(extract_from)
+    in_idx_in_sorted = np.where(np.isin(extract_from[sorted_idx], target_set))[0]
+    sorted_idx_with_in = sorted_idx[in_idx_in_sorted]
+    return sorted_idx_with_in
--- scripts/README.md
+++ scripts/README.md
@@ -6,7 +6,7 @@ Misc of note for scripts in this directory.
 myhdf5=/vol/bmd/yanyul/UKB/predicted_expression/predicted_expression.ukb_imp_x_ctimp_Whole_Blood.h5
 myyaml=/vol/bmd/yanyul/GitHub/ptrs-tf/misc_files/split_ptrs_ukb.yaml
 prefixout=ukb_imp_x_ctimp_Whole_Blood
-screen -dmS split bash run_split_pred_expr.screen $myhdf5 $myyaml $prefixout
+screen -dmS split-$prefixout bash run_split_pred_expr.screen $myhdf5 $myyaml $prefixout
 ```
 
 * `run_least_squared.screen` 
